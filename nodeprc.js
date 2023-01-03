7fc4da6f08aae070227a797fc736ecff7f00093c
ashoka-km <121672755+ashoka-km@users.noreply.github.com>
2023-01-03 16:44:41 +0530

    Add files via upload

---
+++ nodeprc.js
@@ -0,0 +1,17 @@
+
+// User input pragram example
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+rl.question('please enter your name : ' , (name) => {
+    console.log('Yoyy yoyy My boss iss ' + name);
+    rl.close();
+})
+rl.on('close', () => {
+    console.log('Interface closed');
+    process.exit(0);
+})
